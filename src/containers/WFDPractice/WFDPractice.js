790fb07ce001437285e2a98626bbdf5b9055b634
Hoang Huy <huy3041993@gmail.com>
2019-12-26 01:51:27 +1100

    load data from firebase and push data to aws

--- src/containers/WFDPractice/WFDPractice.js
+++ src/containers/WFDPractice/WFDPractice.js
@@ -1,21 +1,63 @@
 import React, { Component } from "react";
 import Player from "../../components/Player/Player";
 import axiosServer from "../../axios/pte-server/axios-pte-server";
+import axiosFirebase from "../../axios/pte-firebase/axios-pte-firebase";
+import _ from "lodash";
 
 class WFDPractice extends Component {
   state = {
-    data: []
+    data: [],
+    questions: {},
+    showAnswer: false
   };
 
   componentDidMount() {
-    this.loadData("test", "test");
+    //https://ptelearning-7266d.firebaseio.com/wfd.json
+    this.loadQuestionBank();
   }
 
+  loadQuestionBank = () => {
+    axiosFirebase
+      .get('/wfd.json?orderBy="$key"&limitToLast=1')
+      .then(response => {
+        console.log(response.data);
+        Object.keys(response.data).map(question => {
+          console.log("question", response.data[question]);
+          const questionObj = response.data[question];
+          this.setState({ questions: questionObj });
+          if (!_.isEmpty(this.state.questions)) {
+            this.loadData("cmt.mp3", this.state.questions[0][0]);
+          }
+          //   if (this.state.questions.length > 0) {
+          //     console.log(this.state.questions[0][0]);
+          //     this.loadData(this.state.questions[0][0]);
+          //   }
+
+          //   console.log(questionObj);
+          //   return Object.keys(questionObj).map((key, i) => {
+          //     console.log(questionObj[key]);
+          // if (key !== "createdDate") {
+          //   console.log(questionObj[key][0]);
+          //   const text = questionObj[key][0];
+          //   return this.loadData(i + ".mp3", text);
+          // }
+          // return null;
+          //   });
+          //   console.log("testing: ", response.data[question]);
+        });
+        // this.setState({ questions: response.data });
+        // console.log("data firebase: ", this.state.data);
+      })
+      .catch(error => {
+        console.log("error inserting");
+      });
+  };
+
   loadData = (fileName, text) => {
+    console.log("load Data from file");
     const params = {
-      fileName: "cmt.mp3",
-      text:
-        "Text messages are used for personal, family, business and social purposes. Governmental and non-governmental organizations use text messaging for communication between colleagues. In the 2010s, the sending of short informal messages has become an accepted part of many cultures"
+      fileName: fileName,
+      text: text
     };
     axiosServer
       .post("/wfd", params)
@@ -32,6 +74,11 @@ class WFDPractice extends Component {
         console.log("error inserting");
       });
   };
+
+  handleShowAnswer = () => {
+    this.setState({ showAnswer: !this.state.showAnswer });
+  };
+
   render() {
     // this.props.updateData(data, cols);
     // this.setState({
@@ -40,15 +87,20 @@ class WFDPractice extends Component {
     return (
       <React.Fragment>
         <div className="text-left">
-          <button className="btn btn-secondary mr-3" type="button">
+          <button
+            className="btn btn-secondary mr-3"
+            type="button"
+            onClick={this.handleShowAnswer}
+          >
             Show Answer
           </button>
-          <div className="alert alert-success" role="alert">
-            <span>
-              <strong>Alert</strong> text.
-              {/* {this.loadData("test", "test")} */}
-            </span>
-          </div>
+          {this.state.showAnswer ? (
+            <div className="alert alert-success" role="alert">
+              <span>testing</span>
+            </div>
+          ) : (
+            ""
+          )}
         </div>
         <Player source={this.state.data}></Player>
       </React.Fragment>
